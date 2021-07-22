b0f4d4d5a1fd6b3a0c5aa4c07669b6279f0ad1f3
Hellol77 <dhe7700@naver.com>
2021-07-23 02:51:16 +0900

    posts template add

--- djangram/posts/views.py
+++ djangram/posts/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return render(request,'posts/index.html')
+	return render(request,'posts/base.html')
+
+def post_create(request):
+	if request.method=='GET':
+		return render(request, 'post/base.html')
+	elif request.method=='POST':
+		pass
